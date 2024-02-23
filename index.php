89f9c90fa1fbf3b1a3521b04ccfe4e24d9a6e0ae
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-02-23 17:10:48 +0100

    added construct

--- index.php
+++ index.php
@@ -10,12 +10,18 @@ class Movie
         {
             echo 'Ciao, sono' . ' ' . $this->title;
         }
+        //Costruttore
+        public function __construct($title, $type, $length)
+        {
+            $this->title= $title;
+            $this->type= $type;
+            $this->length= $length;
+        }
 
     }
 
     //Oggetto/Istanza della classe Movie
-    $movie1 = new Movie();
-    $movie1->title = 'Joker';
+    $movie1 = new Movie('Joker','Action','140 min');
     $movie1->introduce();
 
 
