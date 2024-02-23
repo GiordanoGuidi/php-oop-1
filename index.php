ca568d1763728e90e43850051b42b7e8d0c49dad
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-02-23 18:44:46 +0100

    added class Director

--- index.php
+++ index.php
@@ -1,34 +1,58 @@
 <?php
+//# Classi
+//CLASSE MOVIE
 class Movie
     {
         //variabili di istanza
         public $title;
         public $type;
         public $length;
+        public $director;
         //Funzione
         public function introduce()
         {
             echo 'Ciao, sono' . ' ' . $this->title;
         }
         //Costruttore
-        public function __construct($title, $type, $length)
+        public function __construct($title, $type, $length, Director $director)
         {
             $this->title= $title;
             $this->type= $type;
             $this->length= $length;
+            $this->director=$director;
         }
 
     }
+//CLASSE DIRECTOR
+class Director
+    {
+        public $name;
 
+        //Costruttore
+        public function __construct($name)
+        {
+            $this->name= $name;
+        }
+
+        public function saluta()
+        {
+            echo 'ciao sono'. ' ' .$name;
+        }
+    }
+    //Istanza della classe DIRECTOR
+    $director1 = new Director('Todd Phillips');
+    $director2 = new Director('Tony Scott');
+    // var_dump($director1,$director2);
+
+    //# Istanze delle Classi
     //Oggetto/Istanza della classe Movie
-    $movie1 = new Movie('Joker','Action','140 min');
+    $movie1 = new Movie('Joker','Action','140 min',$director1);
     $movie1->introduce();
-
-
-    $movie2 = new Movie('Top Gun','Action','120 min');
+    //SECONDO OGGETTO MOVIE
+    $movie2 = new Movie('Top Gun','Action','120 min',$director2);
     $movie1->introduce();
+    var_dump($movie1->$director1->saluta(),$movie2);
 
-    var_dump($movie1,$movie2)
 
 
 
